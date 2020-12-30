71d1bbe985e3362211b1f994beb9d2621c78bca0
Valentino Pezzano <valentino_pezzano-github@yahoo.com>
2020-12-30 19:41:35 +0100

    Minor changes

--- src/main/java/model/NewsId2.java
+++ src/main/java/model/NewsId2.java
@@ -1,6 +1,9 @@
 package model;
 
-public class NewsId2  {
+import java.io.Serializable;
+
+public class NewsId2 implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private String title;
 	private String language;
 
